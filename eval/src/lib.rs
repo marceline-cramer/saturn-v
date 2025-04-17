a610f78fbda70e61303cf1cb18c3ec3095538fb7
marceline-cramer <cramermarceline@gmail.com>
2025-04-17 15:43:10 -0600

    License AGPL-3.0

--- eval/src/lib.rs
+++ eval/src/lib.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2025 Marceline Cramer
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Saturn V is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// Saturn V is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Saturn V. If not, see <https://www.gnu.org/licenses/>.
+
 pub mod dataflow;
 pub mod load;
 pub mod solve;
