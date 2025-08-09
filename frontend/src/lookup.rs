697b8006d8ddf42ad02a8648705a2ef6ec5bba93
marceline-cramer <cramermarceline@gmail.com>
2025-08-08 21:56:58 -0600

    frontend: remove lingering trace of NM code

--- frontend/src/lookup.rs
+++ frontend/src/lookup.rs
@@ -224,7 +224,7 @@ pub fn relation_direct_deps<'db>(
     relation_rules(db, rel)
         .into_iter()
         .flat_map(|rule| rule.bodies(db).clone())
-        .flat_map(|body| rule_body_relations(db, body).into_keys())
+        .flat_map(|body| rule_body_relations(db, body))
         .collect()
 }
 
