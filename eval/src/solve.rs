069a66290e17df14bb2b779affb78c92a31a609b
Marceline Cramer <cramermarceline@gmail.com>
2025-08-16 22:31:49 -0600

    eval: custom Send+Sync batsat callbacks

--- eval/src/solve.rs
+++ eval/src/solve.rs
@@ -16,6 +16,7 @@
 
 use std::collections::{BTreeMap, BinaryHeap, HashMap};
 
+use batsat::Callbacks;
 use flume::Sender;
 use rustsat::{
     encodings::{
@@ -31,7 +32,12 @@ use rustsat::{
 use saturn_v_ir::{CardinalityConstraintKind, ConstraintKind, ConstraintWeight};
 use tracing::{debug, span, Level};
 
-pub type Oracle = rustsat_batsat::BasicSolver;
+pub type Oracle = rustsat_batsat::Solver<SolverCallbacks>;
+
+#[derive(Default)]
+pub struct SolverCallbacks;
+
+impl Callbacks for SolverCallbacks {}
 
 use crate::{
     types::{Condition, ConditionalLink, ConstraintGroup, Fact, Gate},
