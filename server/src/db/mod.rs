405599bb44a45b67fc3aca78c4677dc42be03cca
Marceline Cramer <cramermarceline@gmail.com>
2025-10-11 21:15:56 -0600

    server: move db tasks to dedicated file

--- server/src/db/mod.rs
+++ server/src/db/mod.rs
@@ -16,7 +16,7 @@
 
 use std::{
     borrow::Cow,
-    collections::{BTreeMap, BTreeSet, VecDeque},
+    collections::BTreeMap,
     hash::{DefaultHasher, Hash, Hasher},
     path::Path,
 };
@@ -32,6 +32,9 @@ use saturn_v_eval::{
 };
 use saturn_v_ir::{self as ir, RelationIO};
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
+
+#[cfg(test)]
+mod tests;
 
 /// A transaction object for updating and querying the Saturn V server.
 pub trait Transaction {
@@ -561,101 +564,4 @@ pub trait CommitDataflow: Clone {
 pub fn db_error<E: std::error::Error>(err: E) -> ServerError {
     tracing::error!("Database error: {}", err);
     ServerError::DatabaseError
-}
-
-/// A mock implementation of CommitDataflow for testing purposes.
-#[derive(Clone, Default)]
-pub struct MockDataflow {
-    sequence: u64,
-    state: BTreeSet<InputEventKind>,
-    events: VecDeque<Vec<InputEvent>>,
-}
-
-impl CommitDataflow for MockDataflow {
-    fn commit(&mut self, events: Vec<InputEvent>) -> SequenceId {
-        self.sequence += 1;
-        self.events.push_front(events);
-        SequenceId(self.sequence)
-    }
-}
-
-impl MockDataflow {
-    /// Pulls a batch of events from the mock dataflow and validates state.
-    pub fn pull(&mut self) {
-        // pull the batch
-        let batch = self
-            .events
-            .pop_back()
-            .expect("expected a next batch of events");
-
-        // apply batch to state
-        self.apply(batch);
-    }
-
-    /// Applies a batch of events to the mock dataflow and validates state.
-    fn apply(&mut self, batch: Vec<InputEvent>) {
-        for event in batch {
-            use InputEvent::*;
-            match event {
-                Insert(kind) => {
-                    let dup = self.state.insert(kind.clone());
-                    assert!(!dup, "duplicate insert event: {kind:?}");
-                }
-                Remove(kind) => {
-                    let had = self.state.remove(&kind);
-                    assert!(had, "duplicate remove event: {kind:?}");
-                }
-            }
-        }
-    }
-
-    /// Checks the current state of the mock dataflow.
-    pub fn assert_state(&self, expected: impl IntoIterator<Item = InputEventKind>) {
-        assert_eq!(self.state, BTreeSet::from_iter(expected));
-    }
-
-    /// Peeks the next batch.
-    pub fn peek(&self) -> &Vec<InputEvent> {
-        self.events.back().expect("expected a next batch of events")
-    }
-
-    /// Assert that input events have been removed in the next batch.
-    pub fn assert_removed(&self, expected: impl IntoIterator<Item = InputEventKind>) {
-        // pull removed events
-        let got = self
-            .peek()
-            .iter()
-            .cloned()
-            .flat_map(InputEvent::remove)
-            .collect::<BTreeSet<_>>();
-
-        // assert match
-        assert_eq!(got, BTreeSet::from_iter(expected));
-    }
-
-    /// Assert that input events have been inserted in the next batch.
-    pub fn assert_inserted(&self, expected: impl IntoIterator<Item = InputEventKind>) {
-        // pull inserted events
-        let got = self
-            .peek()
-            .iter()
-            .cloned()
-            .flat_map(InputEvent::insert)
-            .collect::<BTreeSet<_>>();
-
-        // assert match
-        assert_eq!(got, BTreeSet::from_iter(expected));
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn create_tx() {
-        let dataflow = MockDataflow::default();
-        let db = Database::temporary(dataflow).unwrap();
-        let _tx = db.transaction().unwrap();
-    }
 }
---
+++ server/src/db/tests.rs
@@ -0,0 +1,111 @@
+// Copyright (C) 2025 Marceline Cramer
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Saturn V is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// Saturn V is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Saturn V. If not, see <https://www.gnu.org/licenses>.
+
+use std::collections::{BTreeSet, VecDeque};
+
+use super::*;
+
+/// A mock implementation of CommitDataflow for testing purposes.
+#[derive(Clone, Default)]
+pub struct MockDataflow {
+    sequence: u64,
+    state: BTreeSet<InputEventKind>,
+    events: VecDeque<Vec<InputEvent>>,
+}
+
+impl CommitDataflow for MockDataflow {
+    fn commit(&mut self, events: Vec<InputEvent>) -> SequenceId {
+        self.sequence += 1;
+        self.events.push_front(events);
+        SequenceId(self.sequence)
+    }
+}
+
+impl MockDataflow {
+    /// Pulls a batch of events from the mock dataflow and validates state.
+    pub fn pull(&mut self) {
+        // pull the batch
+        let batch = self
+            .events
+            .pop_back()
+            .expect("expected a next batch of events");
+
+        // apply batch to state
+        self.apply(batch);
+    }
+
+    /// Applies a batch of events to the mock dataflow and validates state.
+    fn apply(&mut self, batch: Vec<InputEvent>) {
+        for event in batch {
+            use InputEvent::*;
+            match event {
+                Insert(kind) => {
+                    let dup = self.state.insert(kind.clone());
+                    assert!(!dup, "duplicate insert event: {kind:?}");
+                }
+                Remove(kind) => {
+                    let had = self.state.remove(&kind);
+                    assert!(had, "duplicate remove event: {kind:?}");
+                }
+            }
+        }
+    }
+
+    /// Checks the current state of the mock dataflow.
+    pub fn assert_state(&self, expected: impl IntoIterator<Item = InputEventKind>) {
+        assert_eq!(self.state, BTreeSet::from_iter(expected));
+    }
+
+    /// Peeks the next batch.
+    pub fn peek(&self) -> &Vec<InputEvent> {
+        self.events.back().expect("expected a next batch of events")
+    }
+
+    /// Assert that input events have been removed in the next batch.
+    pub fn assert_removed(&self, expected: impl IntoIterator<Item = InputEventKind>) {
+        // pull removed events
+        let got = self
+            .peek()
+            .iter()
+            .cloned()
+            .flat_map(InputEvent::remove)
+            .collect::<BTreeSet<_>>();
+
+        // assert match
+        assert_eq!(got, BTreeSet::from_iter(expected));
+    }
+
+    /// Assert that input events have been inserted in the next batch.
+    pub fn assert_inserted(&self, expected: impl IntoIterator<Item = InputEventKind>) {
+        // pull inserted events
+        let got = self
+            .peek()
+            .iter()
+            .cloned()
+            .flat_map(InputEvent::insert)
+            .collect::<BTreeSet<_>>();
+
+        // assert match
+        assert_eq!(got, BTreeSet::from_iter(expected));
+    }
+}
+
+#[test]
+fn create_tx() {
+    let dataflow = MockDataflow::default();
+    let db = Database::temporary(dataflow).unwrap();
+    let _tx = db.transaction().unwrap();
+}
