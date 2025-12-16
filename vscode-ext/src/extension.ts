770319b75f3192f34df758801451bf28c31b3937
Marceline Cramer <cramermarceline@gmail.com>
2025-12-16 15:00:32 -0700

    vscode: use LSP wasm

--- vscode-ext/src/extension.ts
+++ vscode-ext/src/extension.ts
@@ -1,24 +1,27 @@
+import { LanguageServer } from "saturn-v-lsp";
+import { Readable, Writable } from "stream";
 import * as vscode from "vscode";
-import { Executable, LanguageClient, LanguageClientOptions, ServerOptions } from "vscode-languageclient/node";
+import { LanguageClient, LanguageClientOptions, ServerOptions } from "vscode-languageclient/node";
 
 export async function activate(context: vscode.ExtensionContext) {
-  const serverCommand: Executable = {
-    command: "saturn-v",
-    args: ["lsp"],
-  };
-
-  const serverOptions: ServerOptions = {
-    run: serverCommand,
-    debug: serverCommand,
-  };
+  const server = launchLsp();
 
   const clientOptions: LanguageClientOptions = {
     documentSelector: [{ scheme: "file", language: "kerolox" }]
   };
 
-  const client = new LanguageClient("kerolox", serverOptions, clientOptions);
+  const client = new LanguageClient("kerolox", server, clientOptions, true);
   await client.start();
 
   const dispose = async () => await client.stop();
   context.subscriptions.push({ dispose });
+}
+
+export function launchLsp(): ServerOptions {
+  return async () => {
+    const server = new LanguageServer();
+    const writer = Writable.fromWeb(server.requests);
+    const reader = Readable.fromWeb(server.responses);
+    return { server, writer, reader };
+  };
 }
